372ce177e3ea31b9565730952c6aed7706fb8675
ColeHarvey <70413541+ColeHarvey@users.noreply.github.com>
2020-10-18 19:35:27 -0500

    Servlets are go

---
+++ src/controller/NavigationServlet.java
@@ -0,0 +1,69 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.ListBike;
+
+/**
+ * Servlet implementation class NavigationServlet
+ */
+@WebServlet("/navigationServlet")
+public class NavigationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NavigationServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ListBikeHelper dao = new ListBikeHelper();
+		String act = request.getParameter("doThisToItem");
+		
+		//after all changes, we redirect to viewAllItems servlet
+		//the only time we don't is it they select add or edit item
+		String path = "/viewAllItemsServlet";
+		if (act.equals("delete")) {
+			try {
+				Integer tempId = Integer.parseInt(request.getParameter("id"));
+				ListBike bikeToDelete = dao.searchForBikeById(tempId);
+				dao.deleteBike(bikeToDelete);
+			} catch (NumberFormatException e) {
+				System.out.println("Forgot to select a bike");
+			}
+		} else if (act.equals("edit")) {
+			try {
+				Integer tempId = Integer.parseInt(request.getParameter("id"));
+				ListBike bikeToEdit = dao.searchForBikeById(tempId);
+				request.setAttribute("bikeToEdit", bikeToEdit);
+				path = "/edit-bike.jsp";
+			} catch (NumberFormatException e) {
+				System.out.println("Forgot to select a bike");
+			}
+		} else if (act.equals("add")) {
+			path = "/index.html";
+		}
+		
+		getServletContext().getRequestDispatcher(path).forward(request, response);
+	}
+
+}
